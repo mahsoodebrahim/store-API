bf53b5a2b501867f2b2627965a5821d7691189b0
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-02-04 17:06:07 -0800

    add express-async-errors package to handle errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const errorHandleMiddleware = (err, req, res, next) => {
-  return res.status(req.status).json({ msg: req.message });
+  return res.status(500).json({ msg: err.message });
 };
 
 module.exports = errorHandleMiddleware;
