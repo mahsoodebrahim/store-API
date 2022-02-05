68347c31ce3166c32284d954919a85a5ff636126
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-02-05 13:25:18 -0800

    add fields query

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,14 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("name -price");
+  const products = await Product.find({})
+    .sort("name -price")
+    .select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { name, sort } = req.query;
+  const { name, sort, fields } = req.query;
 
   if (name) {
     req.query.name = { $regex: `${name}`, $options: "i" };
@@ -16,6 +18,10 @@ const getAllProducts = async (req, res) => {
 
   if (sort) {
     results = results.sort(sort.replace(",", " "));
+  }
+
+  if (fields) {
+    results = results.select(fields.replace(",", " "));
   }
 
   const products = await results;
