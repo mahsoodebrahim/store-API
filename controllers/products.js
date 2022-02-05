f164db3313b7b9ee9f4d9439cbf3c7466a8283a0
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-02-05 13:15:43 -0800

    add regex to name query and sorted capability of queries

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,25 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ name: "vase table" });
+  const products = await Product.find({}).sort("name -price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { name, sort } = req.query;
+
+  if (name) {
+    req.query.name = { $regex: `${name}`, $options: "i" };
+  }
+
+  let results = Product.find(req.query);
+
+  if (sort) {
+    results = results.sort(sort.replace(",", " "));
+  }
+
+  const products = await results;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
