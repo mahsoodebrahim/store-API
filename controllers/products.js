5aeb0d3fb3d5c9f7774e2ac925b1fc01c907640c
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-02-05 13:56:18 -0800

    setup pagination

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,9 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({})
     .sort("name -price")
-    .select("name price");
+    .select("name price")
+    .skip(2)
+    .limit(10);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -23,6 +25,12 @@ const getAllProducts = async (req, res) => {
   if (fields) {
     results = results.select(fields.replace(",", " "));
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  results = results.skip(skip).limit(limit);
 
   const products = await results;
 
